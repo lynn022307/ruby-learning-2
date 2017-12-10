8c3350dcef85be583f066f9d48a63af52c92b55f
lynn022307 <lynn022307@gmail.com>
2017-12-10 17:20:14 +0800

    updated

---
+++ 001.rb
@@ -0,0 +1,8 @@
+Learn ruby the hard way #001
+1.puts "Hello World!" #Hello World!
+2.puts "Hello Again" #Hello Again
+3.puts "I like typing this." #I like typing this.
+4.puts "This is fun." #This is fun.
+5.puts "Yay! Printing." #Yay! Printing.
+6.puts "I'd much rather you 'not'." #I'd much rather you 'not'.
+7.puts 'I "said" do not touch this.' #I "said" do not touch this.
--- 003.rb
+++ 003.rb
@@ -2,17 +2,17 @@ Learn ruby the hard way #003
 Exercises about operators and how they works
 use irb in terminal
 
-1. puts "I will not count my chickens:" #I will not count my chickens:
-2. puts "Hens #{25 + 30 / 6}" #Hens 30
-3. puts "Roosters #[100 - 25 * 3 % 4]" #97 
-4. puts "Now I will count the eggs" #Now I will count the eggs
-5. puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #7
-6. puts "Is it true that 3 + 2 < 5 - 7?" #Is it true that 3 + 2 < 5 - 7?
-5. puts 3 + 2 < 5 - 7 #false
-6. puts "what is 3 + 2 #{3 + 2}" #what is 3 + 25 btw#{} meaning ruby operator in string
-7. puts "what is 5 - 7 #{5 - 7}" #what is 5 - 7 -2 
-8. puts "oh that's why it's false" #oh that's why it's false
-9. puts "How about some more" #How about some more
-10. puts "Is it greater #{5 > -2}" #Is it greater true
-11. puts "Is it greater or equal? #{5 >= -2}" #Is it greater or equal? true
-12. puts "Is it less or equal? #{5 <= -2}" #Is it less or equal? false
+puts "I will not count my chickens:" #I will not count my chickens:
+puts "Hens #{25 + 30 / 6}" #Hens 30
+puts "Roosters #[100 - 25 * 3 % 4]" #97 
+puts "Now I will count the eggs" #Now I will count the eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #7
+puts "Is it true that 3 + 2 < 5 - 7?" #Is it true that 3 + 2 < 5 - 7?
+puts 3 + 2 < 5 - 7 #false
+puts "what is 3 + 2 #{3 + 2}" #what is 3 + 25 btw#{} meaning ruby operator in string
+puts "what is 5 - 7 #{5 - 7}" #what is 5 - 7 -2 
+puts "oh that's why it's false" #oh that's why it's false
+puts "How about some more" #How about some more
+puts "Is it greater #{5 > -2}" #Is it greater true
+puts "Is it greater or equal? #{5 >= -2}" #Is it greater or equal? true
+puts "Is it less or equal? #{5 <= -2}" #Is it less or equal? false
