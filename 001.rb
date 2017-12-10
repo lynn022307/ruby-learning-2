4fc409d8eadb2b147a2d7af61f9acd42a589b361
lynn022307 <lynn022307@gmail.com>
2017-12-10 17:22:01 +0800

    ex001

--- 001.rb
+++ 001.rb
@@ -1,8 +1,8 @@
 Learn ruby the hard way #001
-1.puts "Hello World!" #Hello World!
-2.puts "Hello Again" #Hello Again
-3.puts "I like typing this." #I like typing this.
-4.puts "This is fun." #This is fun.
-5.puts "Yay! Printing." #Yay! Printing.
-6.puts "I'd much rather you 'not'." #I'd much rather you 'not'.
-7.puts 'I "said" do not touch this.' #I "said" do not touch this.
+puts "Hello World!" #Hello World!
+puts "Hello Again" #Hello Again
+puts "I like typing this." #I like typing this.
+puts "This is fun." #This is fun.
+puts "Yay! Printing." #Yay! Printing.
+puts "I'd much rather you 'not'." #I'd much rather you 'not'.
+puts 'I "said" do not touch this.' #I "said" do not touch this.
